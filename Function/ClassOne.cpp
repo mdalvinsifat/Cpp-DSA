09b78b5bc9e388e6d8dbf2607cfff05dc6c884c5
mdalvinsifat <mdalvinsifat@gmail.com>
2025-09-19 01:00:43 -0700

    funcation

---
+++ Function/ClassOne.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std ; 
+
+void sayHello () 
+{
+    cout << "Hello world";
+}
+
+int main() {
+sayHello();
+
+    return 0;
+}
---
+++ Function/ClassTwo.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std ; 
+
+int sum (int a , int b )
+{
+    int sums = a + b ; 
+    return sums ; 
+}
+int main() {
+
+cout << sum(10,20);
+    return 0;
+}
